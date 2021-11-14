b9eae6473c14b0a8f6ae969328a88b0fa199ce87
GumUlf <programming@gumulf.se>
2021-11-14 22:15:10 +0100

    First asignment, foundation of webpage

--- LexiconMVC/Controllers/HomeController.cs
+++ LexiconMVC/Controllers/HomeController.cs
@@ -12,5 +12,19 @@ namespace LexiconMVC.Controllers
 		{
 			return View();
 		}
+
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		public IActionResult Projects()
+		{
+			return View();
+		}
 	}
 }
