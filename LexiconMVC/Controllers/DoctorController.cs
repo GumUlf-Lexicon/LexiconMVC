f96b92c27c30c4b583902cb1799195a0fa95c6e2
GumUlf <programming@gumulf.se>
2021-11-14 23:46:58 +0100

    FeverCheck implemented

---
+++ LexiconMVC/Controllers/DoctorController.cs
@@ -0,0 +1,26 @@
+﻿using LexiconMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Controllers
+{
+	public class DoctorController: Controller
+	{
+
+		public IActionResult FeverCheck()
+		{
+			ViewBag.feverMessage = null;
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult FeverCheck(string bodyTemperature)
+		{
+			ViewBag.feverMessage = FeverUtility.CheckForFever(bodyTemperature);
+			return View();
+		}
+	}
+}
---
+++ LexiconMVC/Models/FeverUtility.cs
@@ -0,0 +1,27 @@
+﻿namespace LexiconMVC.Models
+{
+	public class FeverUtility
+	{
+		public static string CheckForFever(string bodyTemperatureString)
+		{
+			if(float.TryParse(bodyTemperatureString, out float bodyTemperature))
+			{
+				if(bodyTemperature >= 38)
+				{
+					return "You have a fever! Drink water and get a lot of rest!";
+				}
+				else if(bodyTemperature < 35)
+				{
+					return "You have got hypothermia! Please get help right now!!! Try and get your temperature up!";
+				}
+				else
+				{
+					return "You do not have a fever.";
+				}
+			}
+
+			return "Wrong numberformat!";
+
+		}
+	}
+}
