9c3898c457002994e6a1d531b4f2977765365a13
GumUlf <programming@gumulf.se>
2021-11-18 10:54:13 +0100

    Created basic structure for people part of page

---
+++ LexiconMVC/Models/CreatePersonViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Models
+{
+	// CreatePersonViewModel
+	// - Use to prevent overposting and to use data
+	// annotations to validate inputs when creating
+	// new person.
+	public class CreatePersonViewModel
+	{
+	}
+}
---
+++ LexiconMVC/Models/PersonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Models
+{
+	// PersonViewModel
+	// - Container for the information you need in
+	// your people view
+	public class PersonViewModel
+	{
+	}
+}
