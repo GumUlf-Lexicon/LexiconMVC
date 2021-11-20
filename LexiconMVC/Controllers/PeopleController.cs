ec5dc3c567630c2051d155ae82bdaa296627c266
GumUlf <programming@gumulf.se>
2021-11-20 03:25:04 +0100

    Implemented people page with list of people, filtering of the list and possibility to add new people.

--- LexiconMVC/Controllers/PeopleController.cs
+++ LexiconMVC/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LexiconMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,73 @@ namespace LexiconMVC.Controllers
 {
 	public class PeopleController: Controller
 	{
+		private static readonly PeopleViewModel pwm = new PeopleViewModel();
+		private static bool isPeoplePopulated = false;
+
 		public IActionResult Index()
 		{
-			return View();
+			if(!isPeoplePopulated)
+			{
+				_ = pwm.PopulatePeople();
+				isPeoplePopulated = true;
+			}
+
+			return View(pwm);
+		}
+
+		public IActionResult RemovePerson(int personId)
+		{
+			int personIndex =	pwm.People.FindIndex(person => person.PersonId == personId);
+
+			if(personIndex >= 0)
+			{
+				pwm.People.RemoveAt(personIndex);
+			}
+
+			return View("Index", pwm);
+		}
+
+		[HttpPost]
+		public IActionResult CreatePerson(CreatePersonViewModel createPersonVM)
+		{
+			ViewBag.modelErrors = null;
+
+			if(ModelState.IsValid)
+			{
+				pwm.People.Add(new Person(createPersonVM.Name, createPersonVM.PhoneNumber, createPersonVM.City));
+				ModelState.Clear();
+			}
+			else
+			{
+				var modelErrors = new List<string>();
+				foreach(var modelState in ModelState.Values)
+				{
+					foreach(var modelError in modelState.Errors)
+					{
+						modelErrors.Add(modelError.ErrorMessage);
+					}
+				}
+
+				ViewBag.modelErrors = modelErrors;
+
+			}
+			return View("Index", pwm);
+		}
+
+
+		[HttpPost]
+		public IActionResult FilterPersons(string filtersString)
+		{
+			if(string.IsNullOrWhiteSpace(filtersString))
+			{
+				pwm.SearchPhrase = null;
+			}
+			else
+			{
+				pwm.SearchPhrase = filtersString;
+			}
+
+			return View("Index", pwm);
 		}
 	}
 }
---
+++ LexiconMVC/Models/Person.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexiconMVC.Models
+{
+
+	// Person
+	// – Person data.
+	public class Person
+	{
+		private static int currentPersonId = 0;
+
+		[DisplayName("Person ID")]
+		public int PersonId { get; private set; }
+
+
+		private string _name;
+
+		[DisplayName("Name")]
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				if(string.IsNullOrWhiteSpace(value))
+					throw new ArgumentException("Person's name cannot be empty!");
+				_name = value;
+			}
+		}
+
+		private string _phoneNumber;
+
+		[DisplayName("Phone number")]
+		public string PhoneNumber
+		{
+			get { return _phoneNumber; }
+			set
+			{
+				if(string.IsNullOrWhiteSpace(value))
+					throw new ArgumentException("Person's phone number cannot be empty!");
+				_phoneNumber = value;
+			}
+		}
+
+
+		private string _city;
+
+		[DisplayName("City")]
+		public string City {
+			get { return _city; }
+			set 
+			{
+				if(string.IsNullOrWhiteSpace(value))
+					throw new ArgumentException("Person's city cannot be empty!");
+				_city = value;
+			}
+		}
+
+		public Person(string name, string phoneNumber, string city)
+		{
+			Name = name;
+			PhoneNumber = phoneNumber;
+			City = city;
+			PersonId = ++currentPersonId;
+		}
+	}
+}
